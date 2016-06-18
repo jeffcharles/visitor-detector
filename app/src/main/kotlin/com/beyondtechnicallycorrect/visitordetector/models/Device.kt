e55087242061a123f4544f0ffac5401f6e23bed0
Jeffrey Charles <jeffreycharles@gmail.com>
2016-06-18 16:51:43 -0400

    Show mac address as last line

--- app/src/main/kotlin/com/beyondtechnicallycorrect/visitordetector/models/Device.kt
+++ app/src/main/kotlin/com/beyondtechnicallycorrect/visitordetector/models/Device.kt
@@ -2,6 +2,6 @@ package com.beyondtechnicallycorrect.visitordetector.models
 
 data class Device(val macAddress: String, val hostName: String?, var description: String) {
     override fun toString(): String {
-        return listOf(macAddress, hostName, description).filterNot { it.isNullOrBlank() }.joinToString( " - " )
+        return listOf(description, hostName, macAddress).filterNot { it.isNullOrBlank() }.joinToString( "\n" )
     }
 }
